79cb38d4e473f90da7859b85d729aad982f6d1a4
Ankita Ghadage <105696384+Ankita-8@users.noreply.github.com>
2026-03-05 16:07:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,24 +26,4 @@ function App() {
 export default App;
 
 
-   {/*<Home/>*/}
-      {/*  const movieNumber =1;
-      {movieNumber===2 ? (
-        < MovieCard movie ={{title: "jab we met " ,release_date : "2000 "}}/>
-      ):(
-        < MovieCard movie ={{title: "mahadev " ,release_date : "2013 "}}/>
-      )}*/} 
-
-     {/*or we can do it like this also
-      
-      
-      < MovieCard movie ={{title: "jab we met " ,release_date : "2000 "}}/>
-      < MovieCard movie ={{title: "mahadev " ,release_date : "2013 "}}/>*/}
-
-
-//props is nothing but the properties that we pass to a function( say component ,coz component has function itself)
-//in jsx we can comment using {/* */} <-- this syntax
-// this is called conditional rendering in react.js the first showed one {movieNumber===2 ?}
-// and you can do it like this also 
-   //  This is called shortcircuiting in javascript : {movieNumber===1 && < MovieCard movie ={{title: "jab we met " ,release_date : "2000 "}}/>}
-   //E:\React_tutorial\frontend\src\App.jsx    E:\React_tutorial\frontend\src\css\App.css
+   
